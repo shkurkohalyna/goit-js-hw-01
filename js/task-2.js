a04f8665494740136726ab48cd8a168d96081271
shkurkohalyna <krysty1105@gmail.com>
2020-08-17 01:08:53 +0300

    dsfd

---
+++ js/task-2.js
@@ -0,0 +1,8 @@
+const total = 100;
+let ordered = 130;
+
+if (ordered > total) {
+  console.log("На складе недостаточно товаров!");
+} else {
+  console.log("Заказ оформлен, с вами свяжется менеджер");
+}
---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+const ADMIN_PASSWORD = prompt("1197");
+let message;
+
+console.log(ADMIN_PASSWORD);
+
+("Отменено пользователем!");
+
+("Добро пожаловать!");
+
+("Доступ запрещен, неверный пароль!");
